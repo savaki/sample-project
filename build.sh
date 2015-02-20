12e6c4a466b7b857e1c0e79c3d1ee6e3f1f96564
savaki <matt.ho@gmail.com>
2015-02-20 12:38:26 -0800

    changed rakefile to bash script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -eu
+
+mkdir -p target/samples
+echo "the time has come the walrus said" > target/samples/sample1.txt 
+echo "to speak of many things" > target/samples/sample2.txt
+
